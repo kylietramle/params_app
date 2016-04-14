204f8577850aeed59ad4c9edb08a9675da8462f1
Kylie Le <kylie.tram.le@gmail.com>
2016-04-13 21:16:58 -0700

    initial commit, send information using params

---
+++ app/controllers/names_controller.rb
@@ -0,0 +1,28 @@
+class NamesController < ApplicationController
+  def index
+    @capitalized_name = params[:name].upcase
+
+    if @capitalized_name.downcase[0] == "a"
+      @message = "Hey, your name starts with the first letter of the alphabet"
+    end
+  end
+
+  def number_game
+    secret = 55
+    user_guess = params[:guess].to_i
+
+    if user_guess < secret
+      @less_message = "Your guess is too low"
+    elsif user_guess > secret
+      @greater_message = "Your guess is too high"
+    else
+      @bingo_message = "Bingo! Your guess is correct!"
+    end
+  end
+
+  def button_count
+    @count = 0
+
+  end
+    
+end
